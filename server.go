22f1fb5a3e961e721ff2990c577e61ce56cc1963
James Morrison <jamesmorrison3@live.co.uk>
2018-05-30 22:36:47 +0100

    Added Dep for dependency management. Included mux router. Added video player on frontend

--- server.go
+++ server.go
@@ -3,22 +3,45 @@ package main
 import (
     "log"
     "net/http"
+    mux "github.com/gorilla/mux"
 )
 
+//dummy function that gets a video filename by ID. In reality we'd do a database call here
+func getVideoById(id string) string {
+	if id == "hello" {
+		return "video.mp4"
+	} else {
+		return ""
+	}
+}
+
 //serve index.html file to client
-func serveIndex(w http.ResponseWriter, r *http.Request){
-  http.ServeFile(w, r, "./index.html")
+func serveIndex(response http.ResponseWriter, request *http.Request){
+  http.ServeFile(response, request, "./index.html")
   log.Print("/ called and index.html served")
 }
 
 //serve video file to client
-func File(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./video.mp4")
+func videoServer(response http.ResponseWriter, request *http.Request) {
+	//get URL variables defined in the router
+	vars := mux.Vars(request)
+
+	//get the filename associated with our ID according to our 'database'
+	filename := getVideoById(vars["id"])
+
+	//serve the file and log
+	http.ServeFile(response, request, filename)
+	log.Printf("Serving video with ID: `%s`, Filename: `%s`", vars["id"], filename)
 }
 
 //initiate the http server with a '/' endpoint which will call the serveIndex function
 func main() {
-    http.HandleFunc("/", serveIndex)
-    http.HandleFunc("/video", File)
+	// Using a router lets us be more flexible with URL variables
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveIndex)
+	router.HandleFunc("/video/{id}", videoServer)
+
+	// the router handles all requests, then passes them along to the appropriate function
+	http.Handle("/", router)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
